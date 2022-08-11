7e10164e07159e9b739022ec5e08e845e697525e
gwonin-kenji <63086285+gwonin-kenji@users.noreply.github.com>
2022-08-11 12:42:57 +0900

    ユーザ一覧ページにユーザカードの作成

--- src/components/pages/UserManagement.tsx
+++ src/components/pages/UserManagement.tsx
@@ -1,5 +1,36 @@
+import { Box, Image, Stack, Text, Wrap, WrapItem } from "@chakra-ui/react";
 import { memo, VFC } from "react";
 
 export const UserManagement: VFC = memo(() => {
-  return <p>ユーザ管理ページです</p>;
+  return (
+    <Wrap p={{ base: 4, md: 10 }}>
+      <WrapItem>
+        <Box
+          w="260px"
+          h="260px"
+          bg="white"
+          borderRadius="10px"
+          shadow="md"
+          p={4}
+          _hover={{ cursor: "pointer", opacity: 0.8 }}
+        >
+          <Stack textAlign="center">
+            <Image
+              borderRadius="full"
+              boxSize="160px"
+              src="https://source.unsplash.com/random"
+              alt="profile photo"
+              m="auto"
+            />
+            <Text fontSize="lg" fontWeight="bold">
+              Mike
+            </Text>
+            <Text fontSize="sm" color="gray">
+              Trout Mike
+            </Text>
+          </Stack>
+        </Box>
+      </WrapItem>
+    </Wrap>
+  );
 });
